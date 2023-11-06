3479d0ac3aab05ac67c1f729d0c73473b1380679
junsnote <wjdwns4552@edu.dju.ac.kr>
2023-11-07 00:12:46 +0900

    Add files chap5

---
+++ kotlin-part1/src/chap05/section4/ThrowExceptionTest.kt
@@ -0,0 +1,20 @@
+package chap05.section4
+
+import java.lang.Exception
+
+fun main() {
+    var amount = 600
+
+    try{
+        amount -= 100
+        checkAmount(amount)
+    }catch (e: Exception){
+        println(e.message)
+    }
+    println("amount: $amount")
+}
+
+fun checkAmount(amount: Int){
+    if(amount < 1000)
+        throw Exception("잔고가 $amount 으로 1000 이하 입니다.")
+}
