2174bd8f825b57d747f3d7cd9c5cc179cb436b7a
junsnote <wjdwns4552@edu.dju.ac.kr>
2023-11-12 22:50:47 +0900

    Add files chap05.section5 RunTest

---
+++ kotlin-part1/src/chap05/section5/RunTest.kt
@@ -0,0 +1,22 @@
+package chap05.section5
+
+fun main() {
+    data class Person(var name: String, var skills: String)
+    var person = Person("Kildong", "Kotlin")
+
+    val returnObj = person.apply{
+        this.name = "Sean"
+        this.skills = "Java"
+        "success"
+    }
+    println(person)
+    println("returnObj: $returnObj")
+
+    val returnObj2 = person.run{
+        this.name = "Dooly"
+        this.skills = "C#"
+        "success"
+    }
+    println(person)
+    println("returnObj2: $returnObj2")
+}
